3446dc815dc0722d931c9f71fb532193efcd249e
ApexHigh <82842270+ApexHigh@users.noreply.github.com>
2023-12-03 19:27:27 +0200

    Coding exercise 7 (#1)
    * Lesson 40
    
    Lesson 40 completed
    
    * Lesson 41
    
    Lesson 41 completed
    
    * Coding Exercise 7
    
    Coding Exercise 7 completed

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 
 import Movies from '../components/Movies';
+import Preloader from '../components/Preloader';
+import Search from '../components/Search';
 
-function Main() {
-	return (
-		<main className="container content">
-			<Movies />
+class Main extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			movies: []
+		}
+
+		this.handleSearch = this.handleSearch.bind(this);
+	}
+
+	handleSearch(searchEndpoint) {
+		fetch(`http://www.omdbapi.com/?apikey=7e3f1687&s=${searchEndpoint}`)
+			.then(result => result.json())
+			.then(data => this.setState({movies: data.Search}));
+	}
+
+	componentDidMount() {
+		fetch('http://www.omdbapi.com/?apikey=7e3f1687&s=matrix')
+			.then(result => result.json())
+			.then(data => this.setState({movies: data.Search}));
+	}
+
+	render() {
+		const { movies } = this.state;
+
+		return <main className='container content'>
+			<Search handleSearch={this.handleSearch}/>
+			{
+				movies.length ? (
+					<Movies movies={movies} />
+				) : <Preloader />
+			}
 		</main>
-	);
+	}
 }
 
 export default Main;
