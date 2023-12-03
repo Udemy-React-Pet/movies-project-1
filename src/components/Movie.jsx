2cc11b31a464f77f20df8314a930bee29e87dee1
ApexHigh <82842270+ApexHigh@users.noreply.github.com>
2023-12-03 19:34:45 +0200

    Revert "Coding exercise 7 (#1)" (#2)
    This reverts commit 3446dc815dc0722d931c9f71fb532193efcd249e.

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 
 function Movie(props) {
-	const {
-		Title: title,
-		Year: year,
-		imdbID: id,
-		Type: type,
-		Poster: poster
-	} = props;
+	const { id, img, title, type, year } = props;
 
 	return (
-		<div id={id} className="card movie">
-			<div className="card-image waves-effect waves-block waves-light">
-				{
-					poster === 'N/A' ? 
-					<img className="activator" src={`https://placehold.co/600x400?text=${title}`} alt=""/> :
-					<img className="activator" src={poster} alt=""/>
-				}
+		<>
+			<div className="row">
+				<div className="col">
+					<div className="card">
+						<div className="card-image">
+							<img src={img}/>
+						</div>
+						<div className="card-content">
+							<div className="card-title">{title}</div>
+							<span>{type}</span>
+							<span className="right">{year}</span>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="card-content">
-				<span className="card-title activator grey-text text-darken-4">{title}</span>
-				<p>{year} <span className="right">{type}</span></p>
-			</div>
-		</div>
+		</>
 	);
 }
 
