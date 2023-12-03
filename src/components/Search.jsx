4e91377d6808a05510147e36c6ae86dac3c77fc5
ApexHigh <82842270+ApexHigh@users.noreply.github.com>
2023-12-03 23:55:09 +0200

    Coding Exercise 8
    Coding Exercise 8 completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -24,6 +24,45 @@ class Search extends React.Component {
 						onKeyDown={(e) => this.handleKey(e)}
 					/>
 					<button className="btn search-btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
+					<div>
+						<p>
+							<label>
+								<input 
+									className="with-gap" 
+									name="card-type" 
+									type="radio" 
+									value="Any" 
+									checked 
+									onChange={(e) => this.props.handleTypeChange(e)}
+								/>
+								<span>Any</span>
+							</label>
+						</p>
+						<p>
+							<label>
+								<input 
+									className="with-gap" 
+									name="card-type" 
+									type="radio" 
+									value="movie" 
+									onChange={(e) => this.props.handleTypeChange(e)} 
+								/>
+								<span>Movies only</span>
+							</label>
+						</p>
+						<p>
+							<label>
+								<input 
+									className="with-gap" 
+									name="card-type" 
+									type="radio" 
+									value="series" 
+									onChange={(e) => this.props.handleTypeChange(e)} 
+								/>
+								<span>Series only</span>
+							</label>
+						</p>
+					</div>
 				</div>
 			</div>
 		);
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -9,20 +9,57 @@ class Main extends React.Component {
 		super();
 
 		this.state = {
-			movies: []
+			movies: [],
+			url: {
+				base: 'http://www.omdbapi.com/?apikey=7e3f1687',
+				search: 'matrix',
+				type: ''
+			}
 		}
 
 		this.searchMovies = this.searchMovies.bind(this);
+		this.handleTypeChange = this.handleTypeChange.bind(this);
 	}
 
 	searchMovies(searchEndpoint) {
-		fetch(`http://www.omdbapi.com/?apikey=7e3f1687&s=${searchEndpoint}`)
+		this.setState((prevState) => ({
+			url: {...prevState.url, search: searchEndpoint}
+		}));
+
+		const { base } = this.state.url;
+
+		fetch(`${base}&s=${searchEndpoint}`)
 			.then(result => result.json())
 			.then(data => this.setState({movies: data.Search}));
 	}
 
+	handleTypeChange(e) {
+		const { base, search } = this.state.url;
+
+		if (e.target.value === 'Any') {
+			this.setState((prevState) => ({
+				url: {...prevState.url, type: ''}
+			}));
+
+
+			fetch(`${base}&s=${search}`)
+			.then(result => result.json())
+			.then(data => this.setState({movies: data.Search}));
+		} else {
+			this.setState((prevState) => ({
+				url: {...prevState.url, type: e.target.value}
+			}));
+
+			fetch(`${base}&s=${search}&type=${e.target.value}`)
+			.then(result => result.json())
+			.then(data => this.setState({movies: data.Search}));
+		}
+	}
+
 	componentDidMount() {
-		fetch('http://www.omdbapi.com/?apikey=7e3f1687&s=matrix')
+		const { base, search } = this.state.url;
+
+		fetch(`${base}&s=${search}`)
 			.then(result => result.json())
 			.then(data => this.setState({movies: data.Search}));
 	}
@@ -31,7 +68,7 @@ class Main extends React.Component {
 		const { movies } = this.state;
 
 		return <main className='container content'>
-			<Search searchMovies={this.searchMovies}/>
+			<Search searchMovies={this.searchMovies} handleTypeChange={this.handleTypeChange}/>
 			{
 				movies.length ? (
 					<Movies movies={movies} />
