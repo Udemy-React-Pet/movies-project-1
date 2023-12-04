6d4ab653d8ba2ccc54a2870fc4a9f66c8ce40b2c
ApexHigh <82842270+ApexHigh@users.noreply.github.com>
2023-12-04 14:23:04 +0200

    Lesson 44
    Lesson 44 completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,79 +1,80 @@
-import React from "react";
+import React from 'react';
 
 class Search extends React.Component {
-	state = {
-		search: '',
-		type: this.props.type
-	};
+  state = {
+    search: '',
+    type: 'all',
+  };
 
-	handleKey = (event) => {
-		const { search, type } = this.state;
+  handleKey = (event) => {
+    if (event.key === 'Enter') {
+      this.props.searchMovies(this.state.search, this.state.type);
+    }
+  };
 
-		if (event.key === 'Enter') {
-			this.props.searchMovies(search, type);
-		}
-	}
+  handleFilter = (event) => {
+    this.setState(() => ({ type: event.target.dataset.type }), () => {
+			this.props.searchMovies(this.state.search, this.state.type);
+		});
+  };
 
-	render() {
-		const { search, type } = this.state;
-
-		return (
-			<div className="row">
-				<div className="input-field">
-					<input
-						className="validate"
-						placeholder="Search"
-						type="search"
-						value={search}
-						onChange={(e) => this.setState({ search: e.target.value })}
-						onKeyDown={(e) => this.handleKey(e)}
-					/>
-					<button className="btn search-btn" onClick={() => this.props.searchMovies(search)}>Search</button>
-					<div>
-						<p>
-							<label>
-								<input 
-									className="with-gap" 
-									name="card-type" 
-									type="radio" 
-									value="Any" 
-									checked={type === ''}
-									onChange={(e) => this.props.handleTypeChange(e)}
-								/>
-								<span>Any</span>
-							</label>
-						</p>
-						<p>
-							<label>
-								<input 
-									className="with-gap" 
-									name="card-type" 
-									type="radio" 
-									value="movie"
-									checked={type === 'movie'} 
-									onChange={(e) => this.props.handleTypeChange(e)} 
-								/>
-								<span>Movies only</span>
-							</label>
-						</p>
-						<p>
-							<label>
-								<input 
-									className="with-gap" 
-									name="card-type" 
-									type="radio" 
-									value="series" 
-									checked={type === 'series'}
-									onChange={(e) => this.props.handleTypeChange(e)} 
-								/>
-								<span>Series only</span>
-							</label>
-						</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='row'>
+        <div className='input-field'>
+          <input
+            className='validate'
+            placeholder='Search'
+            type='search'
+            value={this.state.search}
+            onChange={(e) => this.setState({ search: e.target.value })}
+            onKeyDown={(e) => this.handleKey(e)}
+          />
+          <button
+            className='btn search-btn'
+            onClick={() => this.props.searchMovies(this.state.search, this.state.type)}
+          >
+            Search
+          </button>
+        </div>
+        <div>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='all'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'all'}
+            />
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='movie'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'movie'}
+            />
+            <span>Movies only</span>
+          </label>
+          <label>
+            <input
+              className='with-gap'
+              name='type'
+              type='radio'
+              data-type='series'
+              onChange={this.handleFilter}
+              checked={this.state.type === 'series'}
+            />
+            <span>Series only</span>
+          </label>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -9,66 +9,29 @@ class Main extends React.Component {
 		super();
 
 		this.state = {
-			movies: [],
-			url: {
-				base: 'http://www.omdbapi.com/?apikey=7e3f1687',
-				search: 'matrix',
-				type: ''
-			}
+			movies: []
 		}
 
 		this.searchMovies = this.searchMovies.bind(this);
-		this.handleTypeChange = this.handleTypeChange.bind(this);
 	}
 
-	searchMovies(search, type) {
-		this.setState((prevState) => ({
-			url: {...prevState.url, search: search, type: type}
-		}));
-
-		const { base } = this.state.url;
-
-		fetch(`${base}&s=${search}&type=${type}`)
+	searchMovies(search, type = 'all') {
+		fetch(`http://www.omdbapi.com/?apikey=7e3f1687&s=${search !== '' ? `${search}` : 'matrix'}${type !== 'all' ? `&type=${type}` : ``}`)
 			.then(result => result.json())
 			.then(data => this.setState({movies: data.Search}));
-	}
-
-	handleTypeChange(e) {
-		const { base, search } = this.state.url;
-
-		if (e.target.value === 'Any') {
-			this.setState((prevState) => ({
-				url: {...prevState.url, type: ''}
-			}));
-
-			fetch(`${base}&s=${search}`)
-			.then(result => result.json())
-			.then(data => this.setState({movies: data.Search}));
-		} else {
-			this.setState((prevState) => ({
-				url: {...prevState.url, type: e.target.value}
-			}));
-
-			fetch(`${base}&s=${search}&type=${e.target.value}`)
-			.then(result => result.json())
-			.then(data => this.setState({movies: data.Search}));
-		}
 	}
 
 	componentDidMount() {
-		const { base, search, type } = this.state.url;
-
-		fetch(`${base}&s=${search}&type=${type}`)
+		fetch('http://www.omdbapi.com/?apikey=7e3f1687&s=matrix')
 			.then(result => result.json())
 			.then(data => this.setState({movies: data.Search}));
 	}
 
 	render() {
 		const { movies } = this.state;
-		const { type } = this.state.url;
 
 		return <main className='container content'>
-			<Search searchMovies={this.searchMovies} type={type} handleTypeChange={this.handleTypeChange} />
+			<Search searchMovies={this.searchMovies}/>
 			{
 				movies.length ? (
 					<Movies movies={movies} />
