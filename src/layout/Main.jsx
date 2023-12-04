e1959a383a897376da3265a9a45e8ebee70afec8
ApexHigh <82842270+ApexHigh@users.noreply.github.com>
2023-12-04 21:35:34 +0200

    Lesson 46 (#9)
    * Coding exercise 7 (#1)
    
    * Lesson 40
    
    Lesson 40 completed
    
    * Lesson 41
    
    Lesson 41 completed
    
    * Coding Exercise 7
    
    Coding Exercise 7 completed
    
    * Revert "Coding exercise 7 (#1)" (#2)
    
    This reverts commit 3446dc815dc0722d931c9f71fb532193efcd249e.
    
    * Coding exercise 7 (#3)
    
    * Lesson 40
    
    Lesson 40 completed
    
    * Lesson 41
    
    Lesson 41 completed
    
    * Coding Exercise 7
    
    Coding Exercise 7 completed
    
    * Coding Exercise 8
    
    Coding Exercise 8 completed
    
    * Coding Exercise 8
    
    Coding Exercise 8 completed
    
    * Lesson 44 (#5)
    
    Lesson 44 completed
    
    * Lesson 45 (#6)
    
    * Lesson 44
    
    Lesson 44 completed
    
    * Lesson 45
    
    Lesson 45 completed
    
    * Lesson 46
    
    Lesson 46 completed

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,6 +4,8 @@ import Movies from '../components/Movies';
 import Preloader from '../components/Preloader';
 import Search from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
   constructor() {
     super();
@@ -17,7 +19,7 @@ class Main extends React.Component {
   }
 
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=7e3f1687&s=matrix')
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((result) => result.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
@@ -25,7 +27,7 @@ class Main extends React.Component {
   searchMovies(search, type = 'all') {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=7e3f1687&s=${
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${
         search !== '' ? `${search}` : 'matrix'
       }${type !== 'all' ? `&type=${type}` : ``}`
     )
