79dc0cdaa048642f9717380c2da6c3e37179229e
ApexHigh <82842270+ApexHigh@users.noreply.github.com>
2023-12-08 01:09:30 +0200

    Functional components
    Refactored on functional components

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Movies from '../components/Movies';
 import Preloader from '../components/Preloader';
@@ -6,54 +6,48 @@ import Search from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
-  constructor() {
-    super();
+function Main() {
+	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-    this.state = {
-      movies: [],
-      loading: true,
-    };
-
-    this.searchMovies = this.searchMovies.bind(this);
-  }
-
-  componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
-      .then((result) => result.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
-			.catch((err) => {
-				console.log(err);
-				this.setState({loading: false});
-			});
-  }
-
-  searchMovies(search, type = 'all') {
-    this.setState({ loading: true });
+  const searchMovies = (search, type = 'all') => {
+    setLoading(true);
     fetch(
       `https://www.omdbapi.com/?apikey=${API_KEY}&s=${
         search !== '' ? `${search}` : 'matrix'
       }${type !== 'all' ? `&type=${type}` : ``}`
     )
       .then((result) => result.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => {
+				setMovies(data.Search);
+				setLoading(false);
+			})
 			.catch((err) => {
 				console.log(err);
-				this.setState({loading: false});
+				setLoading(false);
 			});
   }
 
-  render() {
-    const { movies, loading } = this.state;
+	useEffect(() => {
+		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+      .then((result) => result.json())
+      .then((data) => {
+				setMovies(data.Search);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+				setLoading(false);
+			});
+	}, []);
 
-    return (
-      <main className='container content'>
-        <Search searchMovies={this.searchMovies} />
-				<h6>Results found: <span>{movies.length ? movies.length : 0}</span></h6>
-        {loading ? <Preloader /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
+  return (
+		<main className='container content'>
+			<Search searchMovies={searchMovies} />
+			<h6>Results found: <span>{movies.length ? movies.length : 0}</span></h6>
+			{loading ? <Preloader /> : <Movies movies={movies} />}
+		</main>
+	);
 }
 
 export default Main;
